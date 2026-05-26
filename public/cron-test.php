f466ceafceaf2ffe423b9fe8f3855eaf4e25ffe4
smarajit <smarajit.mitra.dev@gmail.com>
2026-05-27 03:05:03 +0530

    Added automatic database backup system

--- public/cron-test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-file_put_contents(
-    dirname(__DIR__) . '/storage/backups/cron-test.txt',
-    "Cron executed at: " . date('Y-m-d H:i:s') . PHP_EOL,
-    FILE_APPEND
-);
-
-echo "Cron Test Success";
